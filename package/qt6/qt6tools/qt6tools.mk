d75567cbc1da905d9ffa07fb8562eaef967bc7d8
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2025-02-18 21:39:09 +0100

    package/qt6/qt6tools: build only host linguist tools
    Strip down qt6tools to the bare minimum by:
    - Removing the target package
    - Disabling all features except for the linguist tools
    
    This change addresses ongoing issues with qt6tools and focuses on its
    primary intended use: providing linguist tools for translating Qt6
    applications during the build process.
    
    The linguist tools are essential for internationalization (i18n) of Qt6
    applications and are now available as a host package for cross-compilation
    purposes.
    
    This modification should resolve stability issues while still maintaining
    the core functionality required for Qt6 application translation.

--- package/qt6/qt6tools/qt6tools.mk
+++ package/qt6/qt6tools/qt6tools.mk
@@ -7,57 +7,48 @@
 QT6TOOLS_VERSION = $(QT6_VERSION)
 QT6TOOLS_SITE = $(QT6_SITE)
 QT6TOOLS_SOURCE = qttools-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6TOOLS_VERSION).tar.xz
-QT6TOOLS_INSTALL_STAGING = YES
 QT6TOOLS_SUPPORTS_IN_SOURCE_BUILD = NO
-
 QT6TOOLS_CMAKE_BACKEND = ninja
-
-QT6TOOLS_LICENSE = \
-	BSD-3-Clause (examples), \
-	BSD-3-Clause (corecon), \
-	BSL-1.0 (catch), \
-	LGPL-3.0 or GPL-3.0 or GPL-2.0, \
-	GPL-3.0 WITH Qt-GPL-exception-1.0
+QT6TOOLS_LICENSE = GPL-3.0 with Qt-GPL-exception-1.0 (host linguist tools)
 
 QT6TOOLS_LICENSE_FILES = \
-	LICENSES/BSD-3-Clause.txt \
-	LICENSES/BSL-1.0.txt \
-	LICENSES/GPL-2.0-only.txt \
 	LICENSES/GPL-3.0-only.txt \
-	LICENSES/LGPL-3.0-only.txt \
 	LICENSES/Qt-GPL-exception-1.0.txt
 
-QT6TOOLS_CONF_OPTS = \
-	-DQT_HOST_PATH=$(HOST_DIR) \
-	-DBUILD_WITH_PCH=OFF \
-	-DQT_BUILD_EXAMPLES=OFF \
-	-DQT_BUILD_TESTS=OFF
-
-QT6TOOLS_DEPENDENCIES = \
-	qt6base \
-	host-qt6tools
-
-ifeq ($(BR2_PACKAGE_QT6DECLARATIVE),y)
-QT6TOOLS_DEPENDENCIES += qt6declarative
-endif
-
+# https://code.qt.io/cgit/qt/qttools.git/tree/configure.cmake?h=6.8.2
 HOST_QT6TOOLS_CONF_OPTS = \
+	-DFEATURE_assistant=OFF \
+	-DFEATURE_attributionsscanner=OFF \
+	-DFEATURE_clang=OFF \
+	-DFEATURE_clang-rtti=OFF \
+	-DFEATURE_clangcpp=OFF \
+	-DFEATURE_designer=OFF \
+	-DFEATURE_distancefieldgenerator=OFF \
+	-DFEATURE_kmap2qmap=OFF \
+	-DFEATURE_pixeltool=OFF \
+	-DFEATURE_qdbus=OFF \
+	-DFEATURE_qdoc=OFF \
+	-DFEATURE_qev=OFF \
+	-DFEATURE_qtattributionsscanner=OFF \
+	-DFEATURE_qtdiag=OFF \
+	-DFEATURE_qtplugininfo=OFF \
 	-DQT_BUILD_EXAMPLES=OFF \
 	-DQT_BUILD_TESTS=OFF
 
 HOST_QT6TOOLS_DEPENDENCIES = host-qt6base
 
-ifeq ($(BR2_PACKAGE_HOST_QT6TOOLS_LINGUIST_TOOLS),y)
-HOST_QT6TOOLS_CONF_OPTS += -DFEATURE_linguist=ON
-# When we have qt6declarative for the target, we need to build the
-# linguist tool with host-qt6declarative support so that it handles
-# QML/JS files
-ifeq ($(BR2_PACKAGE_QT6DECLARATIVE),y)
+ifeq ($(BR2_PACKAGE_HOST_QT6DECLARATIVE),y)
+# https://code.qt.io/cgit/qt/qttools.git/tree/src/linguist/lupdate/CMakeLists.txt?h=6.8.2#n61
 HOST_QT6TOOLS_DEPENDENCIES += host-qt6declarative
 endif
+
+ifeq ($(BR2_PACKAGE_HOST_QT6TOOLS_LINGUIST_TOOLS),y)
+# https://code.qt.io/cgit/qt/qttools.git/tree/configure.cmake?h=6.8.2#n78
+# https://code.qt.io/cgit/qt/qttools.git/tree/src/CMakeLists.txt?h=6.8.2#n26
+# https://code.qt.io/cgit/qt/qttools.git/tree/src/linguist/CMakeLists.txt?h=6.8.2#n8
+HOST_QT6TOOLS_CONF_OPTS += -DFEATURE_linguist=ON
 else
 HOST_QT6TOOLS_CONF_OPTS += -DFEATURE_linguist=OFF
 endif
 
-$(eval $(cmake-package))
 $(eval $(host-cmake-package))
